d7472be37c964d55fa2add06c55b93ce85d751c0
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-04-05 00:29:21 +0200

    build(config): update build config

--- config/grunt/clean.js
+++ config/grunt/clean.js
@@ -1,6 +1,4 @@
 module.exports = {
     'runtime': ['build/web-audio-conference-2018/runtime*.js'],
-    'scripts': ['build/web-audio-conference-2018/!(ngsw-worker).js'],
-    'source-maps': ['build/web-audio-conference-2018/**.map'],
-    'styles': ['build/web-audio-conference-2018/**.css']
+    'source-maps': ['build/web-audio-conference-2018/**.map']
 };
--- config/grunt/copy.js
+++ config/grunt/copy.js
@@ -8,25 +8,5 @@ module.exports = {
                 src: ['404.html']
             }
         ]
-    },
-    scripts: {
-        files: [
-            {
-                cwd: 'build/web-audio-conference-2018/',
-                dest: 'build/web-audio-conference-2018/scripts/',
-                expand: true,
-                src: ['**/!(ngsw-worker).js']
-            }
-        ]
-    },
-    styles: {
-        files: [
-            {
-                cwd: 'build/web-audio-conference-2018/',
-                dest: 'build/web-audio-conference-2018/styles/',
-                expand: true,
-                src: ['**/*.css']
-            }
-        ]
     }
 };
